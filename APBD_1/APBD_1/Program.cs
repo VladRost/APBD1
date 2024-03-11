31a6e56ff21928e683b201caf3f52bb0beed364a
s28288 <s28288@pjwstk.edu.pl>
2024-03-11 13:37:15 +0100

    Implementacja metody

--- APBD_1/APBD_1/Program.cs
+++ APBD_1/APBD_1/Program.cs
@@ -10,6 +10,13 @@
 
     public static void CalculateAvg(int[] arr)
     {
-        
+        double summ = 0;
+        double avg = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            summ += arr[i];
+        }
+
+        avg = summ / arr.Length;
     }
 }
