92ef2dcd59d0b84ddd97df7f44e625c6fa92ea8a
VladRost <s28288@pjwstk.edu.pl>
2024-03-15 10:49:49 +0100

    Findmax impl

--- APBD_1/APBD_1/Program.cs
+++ APBD_1/APBD_1/Program.cs
@@ -5,5 +5,16 @@
         Console.WriteLine("Hello, World!");
         int g = 19;
         int a = 123;
+        int[] arr = {1,2,3,412,23};
+        Console.WriteLine(FindMax(arr));
+    }
+    public static int FindMax(int[] ints){
+        int temp_max = 0;
+        for(int i = 0;i<ints.Length;i++){
+            if (ints[i]>temp_max){
+                temp_max = ints[i];
+            }
+        }
+        return temp_max;
     }
 }
